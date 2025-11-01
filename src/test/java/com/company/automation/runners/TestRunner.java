b5f3ab3305450ed7ae3cd49af3007801577853b3
Ajoy Ashwinth <ajoy.ashwinth.qa@gmail.com>
2025-11-01 21:29:11 +0530

    changes in test runner file to support testng run, tweaks to genertae report and Github actions workflow.

--- src/test/java/com/company/automation/runners/TestRunner.java
+++ src/test/java/com/company/automation/runners/TestRunner.java
@@ -6,13 +6,15 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"com.company.automation"},
+        // Default to @smoke; can be overridden with -Dcucumber.filter.tags
+        tags = "@smoke",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports.html",
                 "json:target/cucumber-report.json",
                 "junit:target/junit-reports/Cucumber.xml"
         }
-        // Note: Use -Dcucumber.filter.tags="@smoke" to run tagged scenarios
+        // Note: Use -Dcucumber.filter.tags="@regression" to run tagged scenarios
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
